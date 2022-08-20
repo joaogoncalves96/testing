20138ddd9d83537896faa8d6acb655e218d43c3a
João Gonçalves <joaogoncalves@Joaos-MBP.home>
2022-08-20 12:06:19 +0100

    first

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'})
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+
+  fireEvent.click(colorButton);
+  expect(colorButton.textContent).toBe('Change to red');
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
 });
+
+test('initial condition', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+})
+
